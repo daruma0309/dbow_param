116f4020c49abe1fdc566d25815e80548abf7949
daruma0309 <ojikan39@gmail.com>
2021-02-09 18:13:17 +0900

    initial commit2

---
+++ include/myUtil.h
@@ -0,0 +1,22 @@
+#include <opencv2/opencv.hpp>
+#include <opencv2/video/tracking.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/features2d/features2d.hpp>
+#include <opencv2/calib3d/calib3d.hpp>
+#include <opencv2/core/mat.hpp>
+#include <opencv2/core/core.hpp>
+
+#include <iostream>
+#include <ctype.h>
+#include <algorithm> // for copy
+#include <iterator> // for ostream_iterator
+#include <vector>
+#include <ctime>
+#include <sstream>
+#include <fstream>
+#include <string>
+#include <cmath>
+
+using namespace cv;
+using namespace std;
---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include "myUtil.h"
+
+using namespace cv;
+using namespace std;
+
+int main( int argc, char** argv )	{
+
+  cout << "End." << endl;
+  return 0;
+}
